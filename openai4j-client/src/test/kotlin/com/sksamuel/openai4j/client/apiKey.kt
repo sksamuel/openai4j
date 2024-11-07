6ac39d743d4182211e19e00fd7595fae41ee80e0
sksamuel <sam@sksamuel.com>
2024-11-06 21:19:41 -0600

    Added key lookup from secrets

--- openai4j-client/src/test/kotlin/com/sksamuel/openai4j/client/apiKey.kt
+++ openai4j-client/src/test/kotlin/com/sksamuel/openai4j/client/apiKey.kt
@@ -1,3 +1,9 @@
 package com.sksamuel.openai4j.client
 
-val apiKey = object {}.javaClass.getResourceAsStream("/openai.key").bufferedReader().readText().trim()
+val apiKey: String
+   get() {
+      val key = System.getenv("OPENAI_API_KEY")
+      return if (key.isNullOrBlank()) {
+         object {}.javaClass.getResourceAsStream("/openai.key").bufferedReader().readText().trim()
+      } else key.trim()
+   }
