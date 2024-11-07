fc91b632b0d1ed2dcb0feecfd0320f63414fcf78
sksamuel <sam@sksamuel.com>
2024-11-06 21:25:54 -0600

    Fixed list models test

--- openai4j-client/src/test/kotlin/com/sksamuel/openai4j/client/ListModelsTest.kt
+++ openai4j-client/src/test/kotlin/com/sksamuel/openai4j/client/ListModelsTest.kt
@@ -9,7 +9,7 @@ class ListModelsTest : FunSpec() {
       test("list models") {
          val openai = VertxOpenAIClient(apiKey)
          val models = openai.listModels().get()
-         models.data.size shouldBe 20
+         models.data.size shouldBe 39
          models.data.forOne {
             it.id shouldBe  "tts-1"
             it.owned_by shouldBe "openai-internal"
