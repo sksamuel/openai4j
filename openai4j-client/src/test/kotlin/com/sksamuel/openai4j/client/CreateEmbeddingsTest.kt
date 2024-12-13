2a9c318412d574cb98925c941baa7a7ca5093521
sksamuel <sam@sksamuel.com>
2024-12-12 22:36:20 -0600

    Updated test

--- openai4j-client/src/test/kotlin/com/sksamuel/openai4j/client/CreateEmbeddingsTest.kt
+++ openai4j-client/src/test/kotlin/com/sksamuel/openai4j/client/CreateEmbeddingsTest.kt
@@ -12,16 +12,16 @@ class CreateEmbeddingsTest : FunSpec() {
             openai.createEmbedding(EmbeddingsRequestV1("embed me please darling", "text-embedding-ada-002")).get()
          resp.data.size shouldBe 1
          resp.data.first().embedding.take(10) shouldBe listOf(
-            -0.044288147,
-            -0.014967259,
-            -0.015220716,
-            -0.013393163,
-            -0.0078838235,
-            0.03302935,
-            -0.008757581,
-            -0.004468834,
-            -0.023224598,
-            -6.6323834E-4
+            -0.044324964,
+            -0.014930655,
+            -0.015197513,
+            -0.013402898,
+            -0.007878956,
+            0.033010226,
+            -0.008779598,
+            -0.0044131493,
+            -0.02317654,
+            -0.0007167615,
          )
          resp.model shouldBe "text-embedding-ada-002"
          resp.usage shouldBe Usage(/* prompt_tokens = */ 4, /* total_tokens = */ 4)
